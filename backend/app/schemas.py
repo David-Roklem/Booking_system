a391c28cb9c353430b842d1c04c277d8ef4c56ad
David-Roklem <roklem@mail.ru>
2025-03-16 12:33:33 +0300

    fix schemas

--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -2,36 +2,18 @@ from pydantic import BaseModel, EmailStr, Field
 from datetime import datetime
 
 
-class BaseModelWithORM(BaseModel):
-    class Config:
-        orm_mode = True
-
-
-class UserCreate(BaseModel):
+class UserCreateSchema(BaseModel):
     email: EmailStr
     password: str
 
 
-class User(BaseModelWithORM):
-    id: int
-    email: EmailStr
-
-
-class Place(BaseModelWithORM):
-    id: int
+class PlaceSchema(BaseModel):
     name: str = Field(max_length=100)
     description: str = Field(max_length=10000)
     capacity: int = Field(gt=0, lt=5000)
 
 
-class BookingCreate(BaseModel):
-    place_id: int
-    start_time: datetime = Field(datetime)
-    end_time: datetime = Field(datetime)
-
-
-class Booking(BaseModelWithORM):
-    id: int
+class BookingSchema(BaseModel):
     user_id: int
     place_id: int
     start_time: datetime = Field(datetime)
